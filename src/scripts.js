a66326b4ee8f8161f71cf42e880f1e349fd7f115
Becca Steinbrecher <beccasteinbrecher@gmail.com>
2020-06-02 15:15:29 -0600

    fix media query style bug

--- src/scripts.js
+++ src/scripts.js
@@ -46,7 +46,7 @@ window.addEventListener("load", fetchData);
 allRecipesBtn.forEach(bt => bt.addEventListener("click", () => domUpdates.showAllRecipes(allRecipes)(allRecipes)));
 savedRecipesBtn.forEach(bt => bt.addEventListener("click", () => domUpdates.showSavedRecipes(allRecipes, recipeData, user)));
 recipesToCkBtn.forEach(bt => bt.addEventListener("click", () => domUpdates.showToCookItems(allRecipes, recipeData, user)));
-filterBtn.forEach(bt => bt.addEventListener("click", () => domUpdates.filterRecipesOnPage(allRecipes, user)));
+filterBtn.forEach(bt => bt.addEventListener("click", () => domUpdates.filterRecipesOnPage(allRecipes, user, recipeData)));
 main.addEventListener("click", () => domUpdates.addToMyRecipes(recipeData, user, fullRecipeInfo, allRecipes, ingredientsData, pantry));
 // pantryBtn.addEventListener("click", () => domUpdates.toggleMenu(menuOpen));
 pantryBtn.forEach(bt => bt.addEventListener("click", toggleMenu));
@@ -60,6 +60,9 @@ mobileSearchForm.addEventListener("submit", () => {searchRecipes(event)});
 // mobileSearchForm.addEventListener("submit", () => {pressEnterSearch(event)});
 menuButton.addEventListener('click', openMobileMenu);
 menuCloseButton.addEventListener('click', closeMobileMenu);
+document.addEventListener('click', function(e) {
+  if (e.target.className === 'instructions') closeMobileMenu()
+})
 
 document.addEventListener('click', function(e) {
   if (e.target && e.target.id === 'minus') {
@@ -103,6 +106,7 @@ function fetchData() {
 
 //MOBILE MEDIA QUERY
 function openMobileMenu() {
+	hidePostForm();
   mobileMenuBody.classList.remove('hide');
   mobileMenuBody.classList.add('menu-body-style');
   mobileMenu.classList.add('open-style');
@@ -116,6 +120,8 @@ function openMobileMenu() {
 }
 
 function openFilterBar() {
+	menuOpen = open;
+	toggleMenu();
   document.querySelector('.mobile-wrap').classList.remove('hide');
   document.querySelector('.filter-btn').addEventListener('click', function() {
     document.querySelector('.mobile-wrap').classList.add('hide');
@@ -202,15 +208,14 @@ function hidePostForm() {
       users = response.wcUsersData
       let id = user.id
       user = new User(users[id - 1])
-      // console.log('user', user)
       pantry = new Pantry(user);
-      console.log('pantryFetch', pantry)
     })
     .then(() => domUpdates.displayPantryInfo(pantry.data, ingredientsData))
     .catch(error => console.log(error))   
 }
 
 function showPostForm() {
+	closeMobileMenu();
   document.getElementById('searched-ingredient-results').innerHTML = ''
   document.getElementById('search-ingredients-input').value = ''
   document.getElementById('post-to-pantry').style.display = 'flex'
@@ -265,12 +270,11 @@ function adjustPantry(ingredID, ingredMod) {
     .then(data => console.log(data))
     .catch(error => console.log(error))
     .then(hidePostForm())
-    // .then(console.log('pantry of user', user.pantry))
-    // .then(console.log('pantry to display', pantry.data))
     .catch(error => console.log(error))
 }
 
 function toggleMenu() {
+	document.querySelector('.mobile-wrap').classList.add('hide');
   var menuDropdown = document.querySelector(".drop-menu");
   menuOpen = !menuOpen;
   if (menuOpen) {
@@ -279,249 +283,3 @@ function toggleMenu() {
     menuDropdown.style.display = "none";
   }
 }
-
-// function capitalize(words) {
-//   return words.split(" ").map(word => {
-//     return word.charAt(0).toUpperCase() + word.slice(1);
-//   }).join(" ");
-// }
-
-// function filterRecipesOnPage() {
-//   if (document.querySelector('.welcome-msg').style.display !== 'none') {
-//     findCheckedBoxes(allRecipes)
-//   }
-//   if (document.querySelector(".my-recipes-banner").style.display !== 'none') {
-//     findCheckedBoxes(user.favoriteRecipes)
-//   }
-//   //if banner is cooknext then
-//   // findCheckedBoxes(user.recipesToCook)
-// }
-
-// function findCheckedBoxes(arr) {
-//   let tagCheckboxes = document.querySelectorAll(".checked-tag");
-//   let checkboxInfo = Array.from(tagCheckboxes)
-//   let selectedTags = checkboxInfo.filter(box => {
-//     return box.checked;
-//   })
-//   domUpdates.findTaggedRecipes(selectedTags, arr);
-// }
-
-//Make these dynamic so we can pass in different arrays to filter through
-//Must have a way to indicate what view we are on => banner?
-
-// function findTaggedRecipes(selected, arr) {
-//   let filteredResults = [];
-//   selected.forEach(tag => {
-//     let recipes = arr.filter(recipe => {
-//       return recipe.tags.includes(tag.id);
-//     });
-//     recipes.forEach(recipe => {
-//       if (!filteredResults.includes(recipe)) {
-//         filteredResults.push(recipe);
-//       }
-//     })
-//   });
-//   showFilteredRecipes(arr)
-//   if (filteredResults.length > 0) {
-//     filterRecipes(filteredResults, arr);
-//   }
-// }
-
-// function showFilteredRecipes(arr) {
-//   showAllRecipes()
-//   if (arr !== allRecipes) {
-//     let unsavedRecipes = recipeData.filter(recipe => {
-//       return !arr.includes(recipe);
-//     });
-//     unsavedRecipes.forEach(recipe => {
-//       let domRecipe = document.getElementById(`${recipe.id}`);
-//       domRecipe.style.display = "none";
-//     });
-//     if (arr === user.favoriteRecipes) {
-//       showMyRecipesBanner()
-//     }
-//     // } else {
-//     //show toDoList banner
-//   }
-// }
-
-// function filterRecipes(filtered, arr) {
-//   let foundRecipes = arr.filter(recipe => {
-//     return !filtered.includes(recipe);
-//   });
-//   hideUnselectedRecipes(foundRecipes)
-// }
-
-// function hideUnselectedRecipes(foundRecipes) {
-//   foundRecipes.forEach(recipe => {
-//     let domRecipe = document.getElementById(`${recipe.id}`);
-//     domRecipe.style.display = "none";
-//   });
-// }
-
-// FAVORITE RECIPE FUNCTIONALITY
-// function addToMyRecipes() {
-//   if (event.target.className === "recipe-icon-card") { domUpdates.addToCookList(recipeData, user) }
-//   else if (event.target.className === "card-apple-icon") { domUpdates.addToFavorites(recipeData, user) }
-//   else if (event.target.id === "exit-recipe-btn") { domUpdates.exitRecipe(fullRecipeInfo) }
-//   else if (event.target.id === "instructions") { openRecipeInfo(event) }
-// }
-
-// function addToCookList() {
-//   let cardId = parseInt(event.target.closest(".recipe-card").id)
-//   let card = recipeData.find(recipe => recipe.id === cardId);
-//   if (!user.recipesToCook.includes(card)) {
-//     event.target.src = "../images/recipeblack.png";
-//     user.saveRecipe(card, 'recipesToCook');
-//   } else {
-//     event.target.src = "../images/recipegreen.png";
-//     user.removeRecipe(card, 'recipesToCook');
-//   }
-// }
-
-// function addToFavorites() {
-//   let cardId = parseInt(event.target.closest(".recipe-card").id)
-//   let card = recipeData.find(recipe => recipe.id === cardId)
-//   if (!user.favoriteRecipes.includes(card)) {
-//     event.target.src = "../images/apple-logo.png";
-//     user.saveRecipe(card, 'favoriteRecipes');
-//   } else {
-//     event.target.src = "../images/apple-logo-outline.png";
-//     user.removeRecipe(card, 'favoriteRecipes');
-//   }
-// }
-
-// function showSavedRecipes() {
-//   showAllRecipes()
-//   let unsavedRecipes = recipeData.filter(recipe => {
-//     return !user.favoriteRecipes.includes(recipe);
-//   });
-//   unsavedRecipes.forEach(recipe => {
-//     let domRecipe = document.getElementById(`${recipe.id}`);
-//     domRecipe.style.display = "none";
-//   });
-//   showMyRecipesBanner();
-// }
-
-// function showToCookItems() {
-//   domUpdates.showAllRecipes(allRecipes);
-//   let unsavedRecipes = recipeData.filter(recipe => {
-//     return !user.recipesToCook.includes(recipe);
-//   });
-//   unsavedRecipes.forEach(recipe => {
-//     let domRecipe = document.getElementById(`${recipe.id}`);
-//     domRecipe.style.display = "none";
-//   });
-//   domUpdates.showToCookBanner()
-// }
-
-// CREATE RECIPE INSTRUCTIONS
-// function openRecipeInfo(event) {
-//   fullRecipeInfo.style.display = "inline";
-//   let recipeId = parseInt(event.target.closest(".recipe-card").id);
-//   let clickedRecipe = allRecipes.find(clickedRecipe => clickedRecipe.id === Number(recipeId));
-//   generateRecipeTitle(clickedRecipe, generateIngredients(clickedRecipe));
-//   addRecipeImage(clickedRecipe);
-//   generateInstructions(clickedRecipe);
-//   fullRecipeInfo.insertAdjacentHTML("beforebegin", "<section id='overlay'></div>");
-// }
-
-// function generateRecipeTitle(clickedRecipe, ingredients) {
-//   const ingredCost = clickedRecipe.calculateIngredCost(ingredientsData);
-//   const ownedIngreds = pantry.checkPantry(clickedRecipe, ingredientsData);
-//   const missingIngreds = pantry.findIngredsMissing(clickedRecipe, ingredientsData);
-
-//   let recipeTitle = `
-//     <button id="exit-recipe-btn">X</button>
-//     <h3 id="recipe-title">${clickedRecipe.name}</h3>
-//     <h4>Ingredients</h4>
-// 		<p>${ingredients}</p>
-// 		<h4>Esimated Cost</h4>
-// 		<p>$${ingredCost}</p>
-// 		<h4>Ingredients You Own</h4>
-// 		<p>${ownedIngreds}</p>
-// 		<h4>Ingredients You're Missing</h4>
-// 		<p>${missingIngreds}</p>`
-//   fullRecipeInfo.insertAdjacentHTML("beforeend", recipeTitle);
-// }
-
-// function addRecipeImage(clickedRecipe) {
-//   document.getElementById("recipe-title").style.backgroundImage = `url(${clickedRecipe.image})`;
-// }
-
-// function generateIngredients(clickedRecipe) {
-//   return clickedRecipe && clickedRecipe.ingredients.map(i => {
-//     let foundIngredient = ingredientsData.find(ingredient => 
-//       ingredient.id === i.id).name;
-//     return `${capitalize(foundIngredient)} (${i.quantity.amount} ${i.quantity.unit})`
-//   }).join(", ");
-// }
-
-// function generateInstructions(clickedRecipe) {
-//   let instructionsList = "";
-//   clickedRecipe.instructions.forEach(i => {
-//     instructionsList += `<li>${i.instruction}</li>`
-//   });
-//   fullRecipeInfo.insertAdjacentHTML("beforeend", "<h4>Instructions</h4>");
-//   fullRecipeInfo.insertAdjacentHTML("beforeend", `<ol>${instructionsList}</ol>`);
-// }
-
-// function exitRecipe() {
-//   while (fullRecipeInfo.firstChild && fullRecipeInfo.removeChild(fullRecipeInfo.firstChild)) {
-
-//   }
-//   fullRecipeInfo.style.display = "none";
-//   document.getElementById("overlay").remove();
-// }
-
-// TOGGLE DISPLAYS
-// function showMyRecipesBanner() {
-//   document.querySelector('.to-cook-banner').style.display = 'none';
-//   document.querySelector(".welcome-msg").style.display = "none";
-//   document.querySelector(".my-recipes-banner").style.display = "block";
-// }
-
-// function showToCookBanner() {
-//   document.querySelector(".my-recipes-banner").style.display = "none";
-//   document.querySelector('.welcome-msg').style.display = 'none';
-//   document.querySelector('.to-cook-banner').style.display = 'block'
-// }
-
-// function showWelcomeBanner() {
-//   document.querySelector('.to-cook-banner').style.display = 'none';
-//   document.querySelector(".my-recipes-banner").style.display = "none";
-//   document.querySelector(".welcome-msg").style.display = "flex";
-// }
-
-// function hideUnsearched(foundRecipes) {
-//   showAllRecipes();
-//   foundRecipes.forEach(recipe => {
-//     let domRecipe = document.getElementById(`${recipe.id}`);
-//     domRecipe.style.display = "none";
-//   });
-// }
-
-// function showAllRecipes() {
-//   allRecipes.forEach(recipe => {
-//     let domRecipe = document.getElementById(`${recipe.id}`);
-//     domRecipe.style.display = "block";
-//   });
-//   showWelcomeBanner();
-// }
-
-// function displaySearchedIngredients(ingredients) {
-//   let results = document.getElementById('searched-ingredient-results')
-//   results.innerHTML = ''
-
-//   ingredients.forEach(ingred => {
-//     results.insertAdjacentHTML('afterbegin', `
-//       <div class="searched-ingredient" id="${ingred.id}">
-//         <div id="add-subtract">
-//           <button id="minus">-</button>
-//           <input id="amount" placeholder="0">
-//           <button id="plus">+</button>
-//         </div>
-//         <p>${ingred.name}</p>
-//       </div>
-//     `)
-//   })
